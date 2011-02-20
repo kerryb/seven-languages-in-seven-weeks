056861f33f0a293e5e5c3139c8dcc34797cd457e
Kerry Buckley <kerryjbuckley@gmail.com>
2011-02-20 19:36:40 +0000

    Ruby 3: method missing

--- ruby/csv.rb
+++ ruby/csv.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 module ActsAsCsv
-  def self.included(base)
+  def self.included base
     base.send :include, InstanceMethods
   end
 
@@ -15,6 +15,20 @@ module ActsAsCsv
         @csv_contents = file.map {|row| row.chomp.split(", ") }
       end
     end
+
+    def each &block
+      csv_contents.map {|row| CsvRow.new headers, row}.each &block
+    end
+
+    class CsvRow
+      def initialize headers, row
+        @values = Hash[*(headers.zip(row).flatten)]
+      end
+
+      def method_missing name
+        @values[name.to_s]
+      end
+    end
   end
 end
 
@@ -22,6 +36,10 @@ class RubyCsv
   include ActsAsCsv
 end
 
-m = RubyCsv.new
-p m.headers
-p m.csv_contents
+csv = RubyCsv.new
+p csv.headers
+p csv.csv_contents
+
+puts
+
+csv.each {|row| puts row.one}
