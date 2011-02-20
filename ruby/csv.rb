656aa9be71ca0fe305965b61c389357d4bd8f6ae
Kerry Buckley <kerryjbuckley@gmail.com>
2011-02-20 12:47:21 +0000

    Simplify code from book

--- ruby/csv.rb
+++ ruby/csv.rb
@@ -2,41 +2,26 @@
 
 module ActsAsCsv
   def self.included(base)
-    base.extend ClassMethods
-  end
-
-  module ClassMethods
-    def acts_as_csv
-      include InstanceMethods
-    end
+    base.send :include, InstanceMethods
   end
 
   module InstanceMethods
-    def read
-      @csv_contents = []
-      filename = self.class.to_s.downcase + '.txt'
-      file = File.new(filename)
-      @headers = file.gets.chomp.split(', ')
-      file.each do |row|
-        @csv_contents << row.chomp.split(', ')
-      end
-    end
-
     attr_accessor :headers, :csv_contents
 
     def initialize
-      read
+      filename = self.class.to_s.downcase + ".txt"
+      File.open filename do |file|
+        @headers = file.gets.chomp.split(", ")
+        @csv_contents = file.map {|row| row.chomp.split(", ") }
+      end
     end
   end
 end
 
 class RubyCsv
-  # no inheritance! You can mix it in
   include ActsAsCsv
-  acts_as_csv
 end
 
 m = RubyCsv.new
-puts m.headers.inspect
-puts m.csv_contents.inspect
-
+p m.headers
+p m.csv_contents
