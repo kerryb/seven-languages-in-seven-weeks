a69c92fcea562790ae86523d08a911479249c315
Kerry Buckley <kerryjbuckley@gmail.com>
2011-08-29 10:00:55 +0100

    Refactor slightly

--- prolog/mysort.pl
+++ prolog/mysort.pl
@@ -4,9 +4,7 @@ mysort([Head|Tail], [Head|SortedTail]) :-
   mysort(Tail, SortedTail),
   [Head1|_] = SortedTail,
   Head =< Head1.
-mysort([Head|Tail], Sorted) :-
-  mysort(Tail, SortedTail),
-  [Head1|Tail1] = SortedTail,
+mysort([Head|Tail], [Head1|SortedRest]) :-
+  mysort(Tail, [Head1|Tail1]),
   Head > Head1,
-  mysort([Head|Tail1], ResortedTail),
-  Sorted = [Head1|ResortedTail].
+  mysort([Head|Tail1], SortedRest).
