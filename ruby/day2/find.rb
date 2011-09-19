4c588461b743a4541b4a0d420f618437c8964420
Kerry Buckley <kerryjbuckley@gmail.com>
2011-09-19 20:47:04 +0100

    Add file with stuff from ruby ch2 "try" section

---
+++ ruby/day2/find.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# without blocks:
+f = File.open __FILE__
+f.each_line {|l| puts l }
+f.close # Easy to forget. What if there's an exception?
+
+# with blocks:
+File.open __FILE__ do |f|
+  f.each_line {|l| puts l }
+end
+
+hash = {:foo => 123, :bar => 456}
+array = hash.to_a
+puts "As an array: #{array.inspect}"
+
+# Possibly the ugliest ruby idiom in the world.
+hash_again = Hash[*(array.flatten)]
+puts "As a hash again: #{hash.inspect}"
+
+hash.each {|k, v| puts "The value of #{k.inspect} is #{v.inspect}" }
+
+# Array as queue:
+queue = []
+%w{foo bar baz}.each {|a| queue.unshift a }
+3.times { puts queue.pop }
