c4f530711dc92bd5eb2e6a22ac111ec38ec2e3af
Kerry Buckley <kerryjbuckley@gmail.com>
2011-11-29 09:41:55 +0000

    Start calculating rows

--- scala/day1/noughts_and_crosses/src/main/scala/Scorer.scala
+++ scala/day1/noughts_and_crosses/src/main/scala/Scorer.scala
@@ -1,5 +1,16 @@
 class Scorer(val board: String) {
-  def finished: Boolean = board contains("O")
+  class Line(val first: Char, val second: Char, val third: Char) {
+    def allNoughts() = first == 'O' && second == 'O' && third == 'O'
+    def allCrosses() = first == 'X' && second == 'X' && third == 'X'
+  }
+
+  def topRow() = new Line(board(0), board(1), board(2))
+  def middleRow() = new Line(board(4), board(5), board(6))
+  def bottomRow() = new Line(board(8), board(9), board(10))
+
+  def rows() = List(topRow, middleRow, bottomRow)
+
+  def finished: Boolean = board.contains("O") || board.contains("X")
 
   def winner: String = {
     if(board contains("O")) {
