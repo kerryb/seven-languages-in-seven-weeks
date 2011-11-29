fe155c54b16348509ee65c6fe68eae4529afcee8
Kerry Buckley <kerryjbuckley@gmail.com>
2011-11-29 10:04:28 +0000

    Detect all wins

--- scala/day1/noughts_and_crosses/src/main/scala/Scorer.scala
+++ scala/day1/noughts_and_crosses/src/main/scala/Scorer.scala
@@ -1,20 +1,31 @@
 class Scorer(val board: String) {
-  class Line(val first: Char, val second: Char, val third: Char) {
-    def allNoughts() = first == 'O' && second == 'O' && third == 'O'
-    def allCrosses() = first == 'X' && second == 'X' && third == 'X'
+  class Line(cells: List[Char]) {
+    def allNoughts() = cells.forall(c => c == 'O')
+    def allCrosses() = cells.forall(c => c == 'X')
   }
 
-  def topRow() = new Line(board(0), board(1), board(2))
-  def middleRow() = new Line(board(4), board(5), board(6))
-  def bottomRow() = new Line(board(8), board(9), board(10))
+  def topRow() = new Line(List(board(0), board(1), board(2)))
+  def middleRow() = new Line(List(board(4), board(5), board(6)))
+  def bottomRow() = new Line(List(board(8), board(9), board(10)))
 
-  def rows() = List(topRow, middleRow, bottomRow)
+  def leftColumn() = new Line(List(board(0), board(4), board(8)))
+  def middleColumn() = new Line(List(board(1), board(5), board(9)))
+  def rightColumn() = new Line(List(board(2), board(6), board(10)))
+
+  def upwardDiagonal() = new Line(List(board(8), board(5), board(2)))
+  def downwardDiagonal() = new Line(List(board(0), board(5), board(10)))
+
+  def lines() = List(topRow, middleRow, bottomRow,
+                     leftColumn, middleColumn, rightColumn,
+                     upwardDiagonal, downwardDiagonal)
 
   def finished: Boolean = board.contains("O") || board.contains("X")
 
   def winner: String = {
-    if(board contains("O")) {
+    if(lines.exists(l => l allNoughts())) {
       "noughts"
+    } else if(lines.exists(l => l allCrosses())) {
+      "crosses"
     } else {
       "no-one"
     }
