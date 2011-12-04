be1939de02285e2f35a9b938af9fd51fd9a7dc9f
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-04 20:24:52 +0000

    Add spaces to specs

--- scala/day2/total_string_length/src/test/scala/StringListUtilsSpec.scala
+++ scala/day2/total_string_length/src/test/scala/StringListUtilsSpec.scala
@@ -2,34 +2,34 @@ import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 
 class StringListUtilsSpec extends Spec with ShouldMatchers {
-  describe("StringListUtils") {
-    describe("totalLength") {
-      describe("for an empty list") {
-        it("is zero") {
+  describe  ("StringListUtils") {
+    describe ("totalLength") {
+      describe ("for an empty list") {
+        it ("is zero") {
           StringListUtils.totalLength(List()) should equal (0)
         }
       }
 
-      describe("for an empty string") {
-        it("is zero") {
+      describe ("for an empty string") {
+        it ("is zero") {
           StringListUtils.totalLength(List("")) should equal (0)
         }
       }
 
-      describe("for all empty strings") {
-        it("is zero") {
+      describe ("for all empty strings") {
+        it ("is zero") {
           StringListUtils.totalLength(List("", "", "")) should equal (0)
         }
       }
 
-      describe("for a single string") {
-        it("is the length of the string") {
+      describe ("for a single string") {
+        it ("is the length of the string") {
           StringListUtils.totalLength(List("foo")) should equal (3)
         }
       }
 
-      describe("for multiple strings") {
-        it("is the total length of the strings") {
+      describe ("for multiple strings") {
+        it ("is the total length of the strings") {
           StringListUtils.totalLength(List("foo", "bar", "quuz")) should equal (10)
         }
       }
