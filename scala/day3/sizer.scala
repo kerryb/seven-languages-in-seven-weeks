ecefa8993ef50005d153190da0f8334a4efaf34d
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-29 10:43:16 +0000

    Keep track of how many more messages we're expecting

--- scala/day3/sizer.scala
+++ scala/day3/sizer.scala
@@ -2,8 +2,10 @@ import scala.io._
 import scala.actors._
 import Actor._
 
+case class GetInfo(url: String)
 case class PageSize(url: String, size: java.lang.Integer)
 case class LinkCount(url: String, count: java.lang.Integer)
+case object NoMoreRequests
 
 object PageLoader {
   def getPage(url : String) = Source.fromURL(url).mkString
@@ -22,17 +24,28 @@ def getPageInfo() = {
   val caller = self
 
   for(url <- urls) {
-    val page = PageLoader.getPage(url)
-    actor { caller ! new PageSize(url, page.size) }
-    actor { caller ! new LinkCount(url, getLinkCount(page)) }
+    caller ! new GetInfo(url)
   }
+  caller ! NoMoreRequests
 
-  for(i <- 1 to urls.size * 2) {
+  var resultsOutstanding = 0
+  var requestsAllReceived = false
+
+  while (!requestsAllReceived || resultsOutstanding > 0) {
     receive {
+      case GetInfo(url) =>
+        resultsOutstanding += 2
+        val page = PageLoader.getPage(url)
+        actor { caller ! new PageSize(url, page.size) }
+        actor { caller ! new LinkCount(url, getLinkCount(page)) }
       case PageSize(url, size) =>
         println(url + " is " + size + " bytes")
+        resultsOutstanding -= 1
       case LinkCount(url, count) =>
         println(url + " has " + count + " links")
+        resultsOutstanding -= 1
+      case NoMoreRequests =>
+        requestsAllReceived = true
     }
   }
 }
