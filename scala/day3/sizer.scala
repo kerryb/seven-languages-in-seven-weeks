7de385e1b92ffb92539022ec1006734c39a0880a
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-30 10:30:51 +0000

    Revert to book version (with sequential stuff removed)
    Just realised that I'd been doing the slow bit (fetching the pages)
    outside the actors, which kind of misses the point!

--- scala/day3/sizer.scala
+++ scala/day3/sizer.scala
@@ -1,54 +1,27 @@
-import java.lang.Integer
 import scala.io._
 import scala.actors._
 import Actor._
 
-case class GetInfo(url: String)
-case class PageSize(url: String, size: Integer)
-case class LinkCount(url: String, count: Integer)
-case object NoMoreRequests
-
 object PageLoader {
-  def getPage(url : String) = Source.fromURL(url).mkString
+  def getPageSize(url : String) = Source.fromURL(url).mkString.length
 }
 
-val urls = List(
-  "http://www.amazon.com/",
+val urls = List("http://www.amazon.com/",
   "http://www.twitter.com/",
   "http://www.google.com/",
-  "http://www.cnn.com/"
-)
-
-def getLinkCount(page: String) = {
-  "<a ".r findAllIn(page) size
-}
+  "http://www.cnn.com/" )
 
 def getPageInfo() = {
   val caller = self
 
   for(url <- urls) {
-    caller ! new GetInfo(url)
+    actor { caller ! (url, PageLoader.getPageSize(url)) }
   }
-  caller ! NoMoreRequests
 
-  var resultsOutstanding = 0
-  var requestsAllReceived = false
-
-  while (!requestsAllReceived || resultsOutstanding > 0) {
+  for(i <- 1 to urls.size) {
     receive {
-      case GetInfo(url) =>
-        resultsOutstanding += 2
-        val page = PageLoader.getPage(url)
-        actor { caller ! new PageSize(url, page.size) }
-        actor { caller ! new LinkCount(url, getLinkCount(page)) }
-      case PageSize(url, size) =>
-        println(url + " is " + size + " bytes")
-        resultsOutstanding -= 1
-      case LinkCount(url, count) =>
-        println(url + " has " + count + " links")
-        resultsOutstanding -= 1
-      case NoMoreRequests =>
-        requestsAllReceived = true
+      case (url, size) =>
+      println("Size for " + url + ": " + size)
     }
   }
 }
