cd9de162d9a8bad9e1b0651c08236e804ad4e211
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-31 14:41:08 +0000

    Word count is 0 for empty string

--- erlang/day1/day1.erl
+++ erlang/day1/day1.erl
@@ -2,7 +2,7 @@
 -export([word_count/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-word_count(_) -> "Not yet implemented".
+word_count("") -> 0.
 
 word_count_test() -> [
     ?assert(word_count("") =:= 0)
