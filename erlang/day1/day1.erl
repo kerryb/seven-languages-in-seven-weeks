ca3b68239571eb1d7f28740c5b9ea6a0787dec66
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-31 17:37:33 +0000

    Handle single tabs and CRs

--- erlang/day1/day1.erl
+++ erlang/day1/day1.erl
@@ -3,6 +3,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 word_count("") -> 0;
+word_count("\n" ++ T) -> word_count(" " ++ T);
+word_count("\t" ++ T) -> word_count(" " ++ T);
 word_count("  " ++ T) -> word_count(" " ++ T);
 word_count(" " ++ T) -> 1 + word_count(T);
 word_count([_]) -> 1;
@@ -12,5 +14,7 @@ word_count_test() -> [
     ?assert(word_count("") =:= 0),
     ?assert(word_count("foo") =:= 1),
     ?assert(word_count("foo bar") =:= 2),
-    ?assert(word_count("foo    bar") =:= 2)
+    ?assert(word_count("foo    bar") =:= 2),
+    ?assert(word_count("foo\tbar") =:= 2),
+    ?assert(word_count("foo\nbar") =:= 2)
   ].
