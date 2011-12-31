5a7e3d120ef1aa0d14b4fde33e77e8bc71f02176
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-31 15:07:34 +0000

    Basic word count (only works with single spaces)

--- erlang/day1/day1.erl
+++ erlang/day1/day1.erl
@@ -2,8 +2,13 @@
 -export([word_count/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-word_count("") -> 0.
+word_count("") -> 0;
+word_count([32|T]) -> 1 + word_count(T);
+word_count([_]) -> 1;
+word_count([_|T]) -> word_count(T).
 
 word_count_test() -> [
-    ?assert(word_count("") =:= 0)
+    ?assert(word_count("") =:= 0),
+    ?assert(word_count("foo") =:= 1),
+    ?assert(word_count("foo bar") =:= 2)
   ].
