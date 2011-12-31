68f1ead2face90dd66cb878a4295f8c367e7f2d3
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-31 14:39:10 +0000

    First failing test

--- erlang/day1/day1.erl
+++ erlang/day1/day1.erl
@@ -1,4 +1,9 @@
 -module(day1).
 -export([word_count/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 word_count(_) -> "Not yet implemented".
+
+word_count_test() -> [
+    ?assert(word_count("") =:= 0)
+  ].
