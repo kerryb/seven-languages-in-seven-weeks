0563ea7274a319006d36ff300681ee3dac96fe58
Kerry Buckley <kerryjbuckley@gmail.com>
2011-12-31 15:09:57 +0000

    Handle multiple spaces

--- erlang/day1/day1.erl
+++ erlang/day1/day1.erl
@@ -3,6 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 word_count("") -> 0;
+word_count([32,32|T]) -> word_count([32|T]);
 word_count([32|T]) -> 1 + word_count(T);
 word_count([_]) -> 1;
 word_count([_|T]) -> word_count(T).
@@ -10,5 +11,6 @@ word_count([_|T]) -> word_count(T).
 word_count_test() -> [
     ?assert(word_count("") =:= 0),
     ?assert(word_count("foo") =:= 1),
-    ?assert(word_count("foo bar") =:= 2)
+    ?assert(word_count("foo bar") =:= 2),
+    ?assert(word_count("foo    bar") =:= 2)
   ].
