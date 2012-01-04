cd60bdff6397d4fff7eba2982dd5d104bde86206
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-04 15:40:15 +0000

    Erlang 2.3: First test for noughts and crosses

---
+++ erlang/day3/noughts_and_crosses.erl
@@ -0,0 +1,7 @@
+-module(noughts_and_crosses).
+-export([state/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+state(_) -> no_winner.
+
+empty_board_test() -> no_winner = state([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']).
