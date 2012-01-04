3b86e25d35005af89908d173e15e6f7dbfe0438d
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-04 09:22:36 +0000

    Erlang 1.2: Pull value for a key out of a hash-like structure

---
+++ erlang/day2/hash.erl
@@ -0,0 +1,13 @@
+-module(hash).
+-export([get/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+get([{Key, Value}|_], Key) -> Value;
+get([_|Tail], Key) -> get(Tail, Key).
+
+get_test() ->
+  Hash = [{erlang, "a functional language"}, {ruby, "an OO language"}],
+  [
+    ?assert("a functional language" =:= hash:get(Hash, erlang)),
+    ?assert("an OO language" =:= hash:get(Hash, ruby))
+  ].
