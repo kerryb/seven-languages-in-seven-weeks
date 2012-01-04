d71c823b8d6bb78f2dae5f09221fd9a3946e55a7
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-04 16:09:56 +0000

    Detect top row noughts win

--- erlang/day3/noughts_and_crosses.erl
+++ erlang/day3/noughts_and_crosses.erl
@@ -2,6 +2,20 @@
 -export([state/1]).
 -include_lib("eunit/include/eunit.hrl").
 
+state([o, o, o|_]) -> o;
 state(_) -> no_winner.
 
-empty_board_test() -> no_winner = state(['', '', '', '', '', '', '', '', '']).
+empty_board_test() -> no_winner = state([
+      '', '', '',
+      '', '', '',
+      '', '', '']).
+
+unfinished_game_test() -> no_winner = state([
+      '',  x,  o,
+       o,  o,  x,
+       x, '', '']).
+
+top_row_noughts_test() -> o = state([
+       o,  o,  o,
+       x,  x, '',
+      '', '', '']).
