e7b35bd8d0b5986c880a1e34a1c2ab7367b1d9ab
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-17 13:08:42 +0000

    Return none if key is not in hash

--- erlang/day2/hash.erl
+++ erlang/day2/hash.erl
@@ -3,11 +3,13 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get([{Key, Value}|_], Key) -> Value;
-get([_|Tail], Key) -> get(Tail, Key).
+get([_|Tail], Key) -> get(Tail, Key);
+get([], _) -> none.
 
 get_test() ->
   Hash = [{erlang, "a functional language"}, {ruby, "an OO language"}],
   [
     ?assert("a functional language" =:= hash:get(Hash, erlang)),
-    ?assert("an OO language" =:= hash:get(Hash, ruby))
+    ?assert("an OO language" =:= hash:get(Hash, ruby)),
+    ?assert(none =:= hash:get(Hash, scala))
   ].
