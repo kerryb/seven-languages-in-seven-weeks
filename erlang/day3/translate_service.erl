1adb66668cd5280c9a4be9ecc7b179d2dfd6fbce
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-23 20:20:31 +0000

    Add start and kill functions

--- erlang/day3/translate_service.erl
+++ erlang/day3/translate_service.erl
@@ -1,5 +1,6 @@
 -module(translate_service).
--export([loop/0, translate/2]).
+-export([start/0, translate/2, kill/1]).
+
 loop() ->
   receive
     {From, "casa"} ->
@@ -10,13 +11,24 @@ loop() ->
       From ! "white",
       loop();
 
+    {From, die} ->
+      From ! "Aargh!",
+      erlang:raise(exit, "Aargh!", []);
+
     {From, _} ->
       From ! "I don't understand.",
       loop()
   end.
 
+start() -> spawn(fun loop/0).
+
 translate(To, Word) ->
   To ! {self(), Word},
   receive
     Translation -> Translation
+  end.
+
+kill(Victim) -> Victim ! {self, die},
+  receive
+    LastWords -> LastWords
   end.
