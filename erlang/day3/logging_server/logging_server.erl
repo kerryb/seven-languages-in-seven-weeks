05f8caa3811ae45b554cf9513bb77a254a1f81e1
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-30 15:47:37 +0000

    OTP logging server (currently just to stdout)

---
+++ erlang/day3/logging_server/logging_server.erl
@@ -0,0 +1,24 @@
+-module(logging_server).
+-behaviour(gen_server).
+
+% Required by gen_server
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+% The actual module API
+-export([start/1, log/1]).
+
+start(File) -> gen_server:start_link({local, ?MODULE}, ?MODULE, [File], []).
+
+log(Message) -> gen_server:call(?MODULE, {Message}). 
+
+init([File]) -> {ok, File}.
+
+handle_call(Message, From, File) ->
+  io:format("~w: ~w.~n", [From, Message]),
+  {reply, ok, File}.
+
+% Required but unused
+handle_cast(_Message, File) -> {noreply, File}.
+handle_info(_Message, File) -> {noreply, File}.
+terminate(_Reason, _File) -> ok.
+code_change(_OldVersion, File, _Extra) -> {ok, File}.
