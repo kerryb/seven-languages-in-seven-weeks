ebc92665abbe2879cb5ba517c05c15132d7c83b7
Kerry Buckley <kerryjbuckley@gmail.com>
2012-01-30 14:28:45 +0000

    Doctor and shooter now monitor each other

--- erlang/day3/doctor/doctor.erl
+++ erlang/day3/doctor/doctor.erl
@@ -1,5 +1,5 @@
 -module(doctor).
--export([loop/0]).
+-export([loop/0, start/0]).
 loop() ->
   process_flag(trap_exit, true),
   receive
@@ -7,9 +7,14 @@ loop() ->
       io:format("Creating and monitoring process.~n"),
       register(revolver, spawn_link(fun roulette:loop/0)),
       loop();
+    die -> exit({doctor,die,at,erlang:time()});
     {'EXIT', From, Reason} ->
       io:format("The shooter ~p died with reason ~p.", [From, Reason]),
       io:format(" Restarting. ~n"),
       self() ! new,
       loop()
   end.
+
+start() ->
+  register(doctor, spawn(fun doctor:loop/0)),
+  doctor ! new.
--- erlang/day3/doctor/roulette.erl
+++ erlang/day3/doctor/roulette.erl
@@ -3,7 +3,17 @@
 
 % send a number, 1-6
 loop() ->
+  process_flag(trap_exit, true),
   receive
     3 -> io:format("bang.~n"), exit({roulette,die,at,erlang:time()});
+    new ->
+      io:format("Creating and monitoring process.~n"),
+      register(doctor, spawn_link(fun doctor:loop/0)),
+      loop();
+    {'EXIT', From, Reason} ->
+      io:format("The doctor ~p died with reason ~p.", [From, Reason]),
+      io:format(" Restarting. ~n"),
+      self() ! new,
+      loop();
     _ -> io:format("click~n"), loop()
   end.
