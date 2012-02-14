81dd74a62a9ed37c67eed521fdac2bebc5408691
Kerry Buckley <kerryjbuckley@gmail.com>
2012-02-14 11:13:57 +0000

    Check function is/isn't called

--- clojure/day2/test/day2/test/core.clj
+++ clojure/day2/test/day2/test/core.clj
@@ -5,7 +5,14 @@
 (defn foo ([] :bar))
 
 (fact "unless when false returns nil"
-  (unless true (foo)) => nil)
+      (unless true (foo)) => nil)
 
-(fact "unless when true returns result of second argument"
-  (unless false (foo)) => :bar)
+(fact "unless when false doesn't call the argument function"
+      (unless true (foo)) => nil
+      (provided
+        (foo) => 42 :times 0))
+
+(fact "unless when true returns result of calling second argument"
+      (unless false (foo)) => 42
+      (provided
+        (foo) => 42))
