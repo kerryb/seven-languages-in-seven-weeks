578c157a86dd7d9ce76be5ceae5445fad5ec5031
Kerry Buckley <kerryjbuckley@gmail.com>
2012-02-26 07:39:27 +0000

    Customers arrive

--- clojure/day3/src/day3/sleeping_barber/callbacks.clj
+++ clojure/day3/src/day3/sleeping_barber/callbacks.clj
@@ -2,6 +2,11 @@
 ;; using a blocking queue.
 (ns day3.sleeping-barber.callbacks)
 
+(def customers-waiting (ref 0))
+
+(defn customer-arrives []
+  (dosync (alter customers-waiting + 1)))
+
 ;; Run (load "day3/sleeping_barber/callbacks")
 ;; (day3.sleeping-barber.callbacks/run) in REPL to simulate ten seconds' worth
 ;; of haircuts
--- clojure/day3/test/day3/test/sleeping_barber/callbacks.clj
+++ clojure/day3/test/day3/test/sleeping_barber/callbacks.clj
@@ -1,3 +1,10 @@
 (ns day3.test.sleeping-barber.callbacks
   (:use [day3.sleeping-barber.callbacks])
   (:use clojure.test midje.sweet))
+
+(fact "When the shop opens there's no-one waiting"
+      (deref customers-waiting) => 0)
+
+(fact "A new customer sits down if there are fewer than three customers waiting"
+      (dorun (repeatedly 3 #(customer-arrives)))
+      (deref customers-waiting) => 3)
