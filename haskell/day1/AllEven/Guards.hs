c8ffa4b12bda0a851481482b3077abee56fa486c
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-04 11:56:28 +0000

    Implement allEven with guards

---
+++ haskell/day1/AllEven/Guards.hs
@@ -0,0 +1,13 @@
+module AllEven.Guards where
+  import Test.QuickCheck
+  import Data.List
+
+  allEven :: [Integer] -> [Integer]
+  allEven [] = []
+  allEven (h:t)
+    | even h = h:allEven t
+    | otherwise = allEven t
+
+  {-tests-}
+  prop_selected_all_even xs = all even (allEven xs)
+  prop_rejected_all_odd xs = all odd (xs \\ allEven xs)
