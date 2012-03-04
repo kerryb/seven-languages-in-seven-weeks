255695554821f8115c40534e7f749aa2cfe7d392
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-04 12:26:04 +0000

    Implement allEven with foldr

---
+++ haskell/day1/AllEven/Foldr.hs
@@ -0,0 +1,11 @@
+module AllEven.Foldr where
+  import Test.QuickCheck
+  import Data.List
+
+  allEven :: [Integer] -> [Integer]
+  allEven (xs) = foldr appendIfEven [] xs
+    where appendIfEven x acc = if even x then x:acc else acc
+
+  {-tests-}
+  prop_selected_all_even xs = all even (allEven xs)
+  prop_rejected_all_odd xs = all odd (xs \\ allEven xs)
