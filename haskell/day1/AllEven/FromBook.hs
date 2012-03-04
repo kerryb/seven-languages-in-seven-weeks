be551e17c180b09654cd7bba5ce04b5a46feb940
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-04 11:56:05 +0000

    Move tests into module

--- haskell/day1/AllEven/FromBook.hs
+++ haskell/day1/AllEven/FromBook.hs
@@ -1,4 +1,11 @@
 module AllEven.FromBook where
-    allEven :: [Integer] -> [Integer]
-    allEven [] = []
-    allEven (h:t) = if even h then h:allEven t else allEven t
+  import Test.QuickCheck
+  import Data.List
+
+  allEven :: [Integer] -> [Integer]
+  allEven [] = []
+  allEven (h:t) = if even h then h:allEven t else allEven t
+
+  {-tests-}
+  prop_selected_all_even xs = all even (allEven xs)
+  prop_rejected_all_odd xs = all odd (xs \\ allEven xs)
