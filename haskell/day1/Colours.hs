8887e353c0281f8548c1c47e3809eee58b2b5152
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-05 20:18:02 +0000

    Haskell 1.3: colour pairs

---
+++ haskell/day1/Colours.hs
@@ -0,0 +1,17 @@
+module Colours where
+  import Test.QuickCheck
+
+  data Colours = Black | Blue | Red | White | Yellow
+    deriving (Enum, Eq, Ord, Show)
+
+  colour_pairs = [(a,b) | a <- colours, b <- colours, a < b] where
+    colours = [Black, Blue, Red, White, Yellow]
+
+  {-tests-}
+
+  prop_expected_values = colour_pairs == [
+    (Black, Blue), (Black, Red), (Black, White), (Black, Yellow),
+    (Blue, Red), (Blue, White), (Blue, Yellow),
+    (Red, White), (Red, Yellow),
+    (White, Yellow)]
+
