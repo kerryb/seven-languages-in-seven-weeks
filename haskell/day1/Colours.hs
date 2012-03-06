5b42ed02ddc50037ed10f9bacdbe3132251869f2
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-06 12:15:00 +0000

    Consistent function naming

--- haskell/day1/Colours.hs
+++ haskell/day1/Colours.hs
@@ -5,19 +5,19 @@ module Colours where
   data Colours = Black | Blue | Red | White | Yellow
     deriving (Eq, Ord, Show)
 
-  colour_pairs = [(a,b) | a <- colours, b <- colours, a < b] where
+  colourPairs = [(a,b) | a <- colours, b <- colours, a < b] where
     colours = [Black, Blue, Red, White, Yellow]
 
   {-tests-}
 
-  prop_expected_values = colour_pairs == [
+  prop_expected_values = colourPairs == [
     (Black, Blue), (Black, Red), (Black, White), (Black, Yellow),
     (Blue, Red), (Blue, White), (Blue, Yellow),
     (Red, White), (Red, Yellow),
     (White, Yellow)]
 
-  prop_all_different = nub colour_pairs == colour_pairs
+  prop_all_different = nub colourPairs == colourPairs
 
-  prop_non_directed = null (intersect colour_pairs [(a, b) | (b, a) <- colour_pairs])
+  prop_non_directed = null (intersect colourPairs [(a, b) | (b, a) <- colourPairs])
 
-  prop_each_pair_different = null [(a, b) | (a, b) <- colour_pairs, a == b]
+  prop_each_pair_different = null [(a, b) | (a, b) <- colourPairs, a == b]
