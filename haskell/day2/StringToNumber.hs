d311e01a19c172096e03de94690892a28e56cec3
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-24 16:01:01 +0000

    Haskell 2.3: string to number (simple numbers)

---
+++ haskell/day2/StringToNumber.hs
@@ -0,0 +1,20 @@
+module StringToNumber where
+  import Test.QuickCheck
+
+  to_number s = foldl (\a b -> 10 * a + (to_digit b)) 0 s
+  to_digit c = case c of
+    '1' -> 1
+    '2' -> 2
+    '3' -> 3
+    '4' -> 4
+    '5' -> 5
+    '6' -> 6
+    '7' -> 7
+    '8' -> 8
+    '9' -> 9
+    otherwise -> 0
+
+  {-tests-}
+
+  prop_empty_is_zero = to_number "" == 0
+  prop_simple_integer = to_number "123" == 123
