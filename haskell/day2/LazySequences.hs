1e4bfa05c24f78959d4333eb21290b3e08703029
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-25 20:23:31 +0100

    Haskell 2.4: combine two sequences

--- haskell/day2/LazySequences.hs
+++ haskell/day2/LazySequences.hs
@@ -1,11 +1,11 @@
 module LazySequences where
   import Test.QuickCheck
 
-  every_third :: Int -> [Int]
   every_third x = (x:every_third (x + 3))
 
-  every_fifth :: Int -> [Int]
   every_fifth x = (x:every_fifth (x + 5))
+
+  every_eighth x y = zipWith (+) (every_third x) (every_fifth y)
 
   {-tests-}
 
@@ -13,4 +13,7 @@ module LazySequences where
     n' = mod n 10000
 
   prop_every_fifth x n = (every_fifth x) !! n' == x + 5 * n' where
+    n' = mod n 10000
+
+  prop_every_eighth x y n = (every_eighth x y) !! n' == x + y + 8 * n' where
     n' = mod n 10000
