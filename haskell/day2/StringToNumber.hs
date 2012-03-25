7cd7d482ff11240418ada7476da805ccc7259965
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-25 10:01:58 +0100

    Haskell 2.3: string to number working at last

--- haskell/day2/StringToNumber.hs
+++ haskell/day2/StringToNumber.hs
@@ -1,8 +1,14 @@
 module StringToNumber where
   import Test.QuickCheck
 
-  to_number s = foldl (\a b -> 10 * a + (to_digit b)) 0 digits where
-    digits = (filter (\x -> elem x ['0'..'9']) s)
+  to_number :: [Char] -> Double
+  to_number s = number / 10 ** (fromIntegral decimal_places) where
+    number = foldl (\a b -> 10 * a + (to_digit b)) 0 digits
+    decimal_places = if '.' `elem` s
+      then length (takeWhile (/= '.') (reverse s))
+      else 0
+    digits = (filter is_digit s)
+    is_digit = (\x -> elem x ['0'..'9'])
     to_digit c = case c of
       '1' -> 1
       '2' -> 2
@@ -20,3 +26,6 @@ module StringToNumber where
   prop_empty_is_zero = to_number "" == 0
   prop_simple_integer = to_number "123" == 123
   prop_ignore_punctuation = to_number "$123,456" == 123456
+  prop_supports_decimals = to_number "123.456" == 123.456
+  prop_handles_zeroes = to_number "0001230.0450" == 1230.045
+  prop_all_at_once = to_number "£12,000.99" == 12000.99
