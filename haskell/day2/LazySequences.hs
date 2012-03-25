02e9a7eae6ababbb9ab789039134d5e7ca9d01ce
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-25 20:16:46 +0100

    Haskell 2.4: lazy 'every fifth' sequence

--- haskell/day2/LazySequences.hs
+++ haskell/day2/LazySequences.hs
@@ -4,7 +4,13 @@ module LazySequences where
   every_third :: Int -> [Int]
   every_third x = (x:every_third (x + 3))
 
+  every_fifth :: Int -> [Int]
+  every_fifth x = (x:every_fifth (x + 5))
+
   {-tests-}
 
   prop_every_third x n = (every_third x) !! n' == x + 3 * n' where
+    n' = mod n 10000
+
+  prop_every_fifth x n = (every_fifth x) !! n' == x + 5 * n' where
     n' = mod n 10000
