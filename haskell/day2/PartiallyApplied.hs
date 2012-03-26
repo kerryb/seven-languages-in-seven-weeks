4ea9ccdf9a20de302c1389607ef465028cee3480
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-26 07:37:19 +0100

    Haskell 2.5: partially applied 'append newline' function

--- haskell/day2/PartiallyApplied.hs
+++ haskell/day2/PartiallyApplied.hs
@@ -3,6 +3,11 @@ module PartiallyApplied where
 
   half = (*) 0.5
 
+  add_newline = append '\n' where
+    append a b = b ++ [a]
+
   {-tests-}
 
   prop_half x = half x == x / 2
+
+  prop_add_newline s = add_newline s == s ++ "\n"
