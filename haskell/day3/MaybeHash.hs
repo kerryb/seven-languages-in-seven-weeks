539e89c9967f785228612310b77ee277d01b8c6f
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-28 14:33:18 +0100

    Haskell 3.1: Custom show function for Hash

--- haskell/day3/MaybeHash.hs
+++ haskell/day3/MaybeHash.hs
@@ -1,15 +1,20 @@
-module Main where
+module MaybeHash where
   import Test.QuickCheck
 
-  data Hash a b = Hash [Pair a b] deriving (Show)
-  data Pair a b = Value a b | Nested a (Hash a b) deriving (Show)
+  data Hash a b = Hash [Pair a b]
+  instance (Show a, Show b) => Show (Hash a b) where
+    show (Hash c) = show c
+
+  data Pair a b = Value a b | Nested a (Hash a b)
+  instance (Show a, Show b) => Show (Pair a b) where
+    show (Value a b) = show a ++ " --> " ++ show b
+    show (Nested a b) = show a ++ " --> " ++ show b
+
   (-->) k v = Value k v
   (-=>) k v = Nested k v
 
-  hash = Hash ["foo" --> 1, "bar" --> 2, "baz" -=> Hash ["quz" --> 3, "quuz" --> 4]]
-
-  main :: IO ()
-  main = do putStrLn $ show $ hash
-
   {-tests-}
 
+  hash = Hash ["foo" --> 1, "bar" --> 2, "baz" -=> Hash ["quz" --> 3, "quuz" --> 4]]
+
+  prop_show = show hash == "[\"foo\" --> 1,\"bar\" --> 2,\"baz\" --> [\"quz\" --> 3,\"quuz\" --> 4]]"
