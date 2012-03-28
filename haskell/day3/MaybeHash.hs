447d98c90e19c87f48b94a4278ecf22540244aa6
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-28 21:49:29 +0100

    Haskell 3.1: Simple fetch (nested fetch fails)

--- haskell/day3/MaybeHash.hs
+++ haskell/day3/MaybeHash.hs
@@ -1,4 +1,4 @@
-module MaybeHash (Hash, (-->), (-=>)) where
+module MaybeHash (Hash, (-->), (-=>), fetch) where
   import Test.QuickCheck
 
   data Hash a b = Hash [Pair a b]
@@ -13,8 +13,16 @@ module MaybeHash (Hash, (-->), (-=>)) where
   (-->) k v = Value k v
   (-=>) k v = Nested k (Hash v)
 
+  fetch :: (Eq a) => a -> Hash a b -> Maybe b
+  fetch _ (Hash []) = Nothing
+  fetch k (Hash ((Value k1 v):t)) = if k == k1
+    then Just v
+    else fetch k (Hash t)
+
   {-tests-}
 
   hash = Hash ["foo" --> 1, "bar" --> 2, "baz" -=> ["quz" --> 3, "quuz" --> 4]]
 
   prop_show = show hash == "[\"foo\" --> 1,\"bar\" --> 2,\"baz\" --> [\"quz\" --> 3,\"quuz\" --> 4]]"
+  prop_fetch_top_level = fetch "foo" hash == Just 1
+  prop_fetch_top_level_missing = fetch "wibble" hash == Nothing
