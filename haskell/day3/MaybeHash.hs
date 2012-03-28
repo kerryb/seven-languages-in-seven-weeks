b186adebc3377accc1b205fdb4f39140fb4ce5bb
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-28 22:14:44 +0100

    Haskell 3.1: Fetch returns Nothing if key not found

--- haskell/day3/MaybeHash.hs
+++ haskell/day3/MaybeHash.hs
@@ -1,11 +1,11 @@
 module MaybeHash (Hash, (-->), (-=>), fetch) where
   import Test.QuickCheck
 
-  data Hash a b = Hash [Pair a b]
+  data Hash a b = Hash [Pair a b] deriving (Eq)
   instance (Show a, Show b) => Show (Hash a b) where
     show (Hash c) = show c
 
-  data Pair a b = Value a b | Nested a (Hash a b)
+  data Pair a b = Value a b | Nested a (Hash a b) deriving (Eq)
   instance (Show a, Show b) => Show (Pair a b) where
     show (Value a b) = show a ++ " --> " ++ show b
     show (Nested a b) = show a ++ " --> " ++ show b
@@ -13,10 +13,13 @@ module MaybeHash (Hash, (-->), (-=>), fetch) where
   (-->) k v = Value k v
   (-=>) k v = Nested k (Hash v)
 
-  fetch :: (Eq a) => a -> Hash a b -> Maybe b
+  fetch :: (Eq a) => a -> Hash a b -> Maybe (Either (Hash a b) b)
   fetch _ (Hash []) = Nothing
   fetch k (Hash ((Value k1 v):t)) = if k == k1
-    then Just v
+    then Just $ Right v
+    else fetch k (Hash t)
+  fetch k (Hash ((Nested k1 v):t)) = if k == k1
+    then Just $ Left v
     else fetch k (Hash t)
 
   {-tests-}
@@ -24,5 +27,5 @@ module MaybeHash (Hash, (-->), (-=>), fetch) where
   hash = Hash ["foo" --> 1, "bar" --> 2, "baz" -=> ["quz" --> 3, "quuz" --> 4]]
 
   prop_show = show hash == "[\"foo\" --> 1,\"bar\" --> 2,\"baz\" --> [\"quz\" --> 3,\"quuz\" --> 4]]"
-  prop_fetch_top_level = fetch "foo" hash == Just 1
+  prop_fetch_top_level = fetch "foo" hash == Just (Right 1)
   prop_fetch_top_level_missing = fetch "wibble" hash == Nothing
